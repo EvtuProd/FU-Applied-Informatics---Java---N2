ce3e7a42f1b3a240144ef2f53dc9e37ee19dc6a5
EvtuProd <mail@tuhachewscky.ru>
2024-05-03 01:27:24 +0300

    Добавлены комментарии

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,19 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
+        // Чтение данных из файла и создание списка голосующих
         List<Voter> voters = readVotersFromFile("input.csv");
+
+        // Вычисление количества голосов за каждый вариант
         Map<Integer, Integer> scores = calculateScores(voters);
-        List<List<Integer>> result = condorcetOrder(scores);
+
+        // Определение порядка предпочтений по методу Кондорсё
+        List<Integer> result = condorcetOrder(scores);
+
+        // Запись результата в файл
         writeResultToFile(result, "result.txt");
+
+        // Вывод результатов голосования в консоль
         System.out.println("Voting Results:");
         for (Map.Entry<Integer, Integer> entry : scores.entrySet()) {
             System.out.println("Option " + entry.getKey() + ": " + entry.getValue() + " votes");
@@ -15,6 +24,7 @@ public class Main {
         System.out.println("Condorcet Order: " + result);
     }
 
+    // Метод для чтения данных из файла и создания списка голосующих
     private static List<Voter> readVotersFromFile(String fileName) {
         List<Voter> voters = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
@@ -33,6 +43,7 @@ public class Main {
         return voters;
     }
 
+    // Метод для вычисления количества голосов за каждый вариант
     private static Map<Integer, Integer> calculateScores(List<Voter> voters) {
         Map<Integer, Integer> scores = new HashMap<>();
         for (Voter voter : voters) {
@@ -45,36 +56,26 @@ public class Main {
         return scores;
     }
 
-    private static List<List<Integer>> condorcetOrder(Map<Integer, Integer> scores) {
-        Map<Integer, List<Integer>> scoreGroups = new HashMap<>();
-        for (Map.Entry<Integer, Integer> entry : scores.entrySet()) {
-            int score = entry.getValue();
-            scoreGroups.computeIfAbsent(score, k -> new ArrayList<>()).add(entry.getKey());
-        }
+    // Метод для определения порядка предпочтений по методу Кондорсё
+    private static List<Integer> condorcetOrder(Map<Integer, Integer> scores) {
+        List<Map.Entry<Integer, Integer>> list = new ArrayList<>(scores.entrySet());
+        list.sort((o1, o2) -> o2.getValue().compareTo(o1.getValue()));
 
-        List<List<Integer>> result = new ArrayList<>(scoreGroups.values());
-        result.sort((group1, group2) -> Integer.compare(scores.get(group2.get(0)), scores.get(group1.get(0))));
+        List<Integer> result = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : list) {
+            result.add(entry.getKey());
+        }
         return result;
     }
 
-    private static void writeResultToFile(List<List<Integer>> result, String fileName) {
+    // Метод для записи результата в файл
+    private static void writeResultToFile(List<Integer> result, String fileName) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
-            for (List<Integer> group : result) {
-                if (result.indexOf(group) > 0) {
+            for (int i = 0; i < result.size(); i++) {
+                if (i > 0) {
                     writer.write(",");
                 }
-                if (group.size() == 1) {
-                    writer.write(String.valueOf(group.get(0)));
-                } else {
-                    writer.write("[");
-                    for (int i = 0; i < group.size(); i++) {
-                        if (i > 0) {
-                            writer.write(",");
-                        }
-                        writer.write(String.valueOf(group.get(i)));
-                    }
-                    writer.write("]");
-                }
+                writer.write(String.valueOf(result.get(i)));
             }
         } catch (IOException e) {
             e.printStackTrace();
